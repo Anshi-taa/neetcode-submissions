92f5125b836f03e83bf335763f7c1a2e419a1f13
Anshita Verma <105321655+Anshi-taa@users.noreply.github.com>
2026-05-06 01:54:24 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/house-robber/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int rob(int[] nums) {
+         int n = nums.length;
+        int [] dp = new int[n];
+        Arrays.fill(dp,-1);
+        if (n==0) return 0;
+
+         dp[0] = nums[0];
+
+        for (int i =1 ; i<n ; i++){
+
+            int take = nums[i];
+            if(i>1) take+= dp[i-2];
+
+            int nottake =0+ dp[i-1];
+
+            dp[i] = Math.max(take, nottake);
+        }
+
+        return dp[n-1];
+        
+    }
+}
